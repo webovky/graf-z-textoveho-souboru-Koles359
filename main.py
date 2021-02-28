b070c3a6dc4c682025086a6b2d4bfd9d21b54202
Petr Kolesár <kolesar.p2@seznam.cz>
2021-02-28 18:43:30 +0100

    commit

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
 
+import pylab
+x = []
+y = []
 with open("soubor.txt", "r") as f:
-    pass
+    lines = f.readlines()
+    for line in lines:
+        x.append(float(line[0:15].strip()))
+        y.append(float(line[17:35].strip()))
+
+
+pylab.plot(x,y)
+pylab.show()
